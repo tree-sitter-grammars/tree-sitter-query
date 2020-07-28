f62003d2f24083610f9a2846b86454f3db3d2f5e
Steven Sojka <steelsojka@gmail.com>
2020-07-28 07:01:18 -0500

    chore(cleanup): cleanup a little bit

--- grammar.js
+++ grammar.js
@@ -3,32 +3,26 @@ module.exports = grammar({
   rules: {
     program: $ => repeat($._definition),
     _definition: $ =>
-      choice($.named_node, $.anonymous_node, $.grouping, $.predicate, $.list),
+      choice(
+        $.named_node,
+        $.anonymous_node,
+        $.grouping,
+        $.predicate,
+        $.list,
+        $.comment
+      ),
     _field_name: $ => seq($.identifier, ":"),
     predicate_type: $ => choice("?", "!"),
     quantifier: $ => choice("*", "+", "?"),
     field_definition: $ => seq(field("name", $._field_name), $._definition),
-    identifier: $ => /[a-z_]+/,
-    capture_identifier: $ => /[a-zA-Z0-9.\-_\$&*]+/,
-    capture: $ => seq("@", field("name", $.capture_identifier)),
+    identifier: $ => /[a-zA-Z0-9.\-_\$#]+/,
+    capture: $ => seq("@", field("name", $.identifier)),
     string: $ => /".*"/,
     parameters: $ => repeat1(choice($.capture, $.string)),
-    list: $ =>
-      seq(
-        "[",
-        repeat($._definition),
-        "]",
-        optional(field("quantifier", $.quantifier)),
-        captures($)
-      ),
+    comment: $ => seq(";", /.*/),
+    list: $ => seq("[", repeat($._definition), "]", quantifier($), captures($)),
     grouping: $ =>
-      seq(
-        "(",
-        repeat($._definition),
-        ")",
-        optional(field("quantifier", $.quantifier)),
-        captures($)
-      ),
+      seq("(", repeat($._definition), ")", quantifier($), captures($)),
     anonymous_node: $ =>
       seq(/".*"/, optional(field("quantifier", $.quantifier)), captures($)),
     named_node: $ =>
@@ -37,7 +31,7 @@ module.exports = grammar({
         field("name", $.identifier),
         repeat(choice($._definition, $.field_definition)),
         ")",
-        optional(field("quantifier", $.quantifier)),
+        quantifier($),
         captures($)
       ),
     predicate: $ =>
@@ -52,4 +46,8 @@ module.exports = grammar({
 
 function captures($) {
   return optional(repeat($.capture));
+}
+
+function quantifier($) {
+  return optional(field("quantifier", $.quantifier));
 }
