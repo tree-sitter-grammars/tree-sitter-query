b0e4acb2515e1d8bc77a759f9826c887a7de14d1
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-10-05 13:06:05 -0400

    SPM support

---
+++ bindings/swift/TreeSitterQuery/query.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_QUERY_H_
+#define TREE_SITTER_QUERY_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_query();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_QUERY_H_
