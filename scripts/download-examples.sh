d3b76ecaccf49e8adfe869ca6ac79c552ecba981
ObserverOfTime <chronobserver@disroot.org>
2024-02-13 11:33:07 +0200

    chore: improve tests

--- scripts/download-examples.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git clone --depth=1 https://github.com/nvim-treesitter/nvim-treesitter .tests/nvim-treesitter
-git clone --depth=1 https://github.com/nvim-treesitter/nvim-treesitter-textobjects .tests/nvim-treesitter-textobjects
-# `tree-sitter parse` uses the extension of the file to detect the parser to be used.
-for f in .tests/*/queries/**/*.scm; do
-  mv "$f" "${f%.scm}.query";
-done
