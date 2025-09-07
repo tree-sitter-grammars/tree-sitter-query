60e253d3c9d6b1131a0f75c85e4bdcc9a48d5b42
Christian Clason <c.clason@uni-graz.at>
2025-09-07 11:12:11 +0200

    release v0.7.0

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ endif
 
 LANGUAGE_NAME := tree-sitter-query
 HOMEPAGE_URL := https://github.com/tree-sitter-grammars/tree-sitter-query
-VERSION := 0.6.2
+VERSION := 0.7.0
 
 # repository
 SRC_DIR := src
--- src/parser.c
+++ src/parser.c
@@ -6396,8 +6396,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_query(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 6,
-      .patch_version = 2,
+      .minor_version = 7,
+      .patch_version = 0,
     },
   };
   return &language;
